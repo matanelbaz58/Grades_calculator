160d871696d6bfab931f4de15d6b63ea98fd2a38
matanelbaz58 <104138539+matanelbaz58@users.noreply.github.com>
2022-08-16 12:50:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,10 @@ public class Main {
         JFrame frame = new JFrame();
         AllCourse allCourse = new AllCourse(8);
 
-        //JFrame frame = new JFrame();
-        //Course mat = new Course();
-        //frame.add(mat);
+
         allCourse.setSize(new Dimension(300,300));
         frame.add(allCourse);
-        //allCourse.setResizable(false);
+        
         frame.setVisible(true);
     }
 }
